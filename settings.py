dfd1dffa8375451beb87a59e4eece73159aed976
Fathima nasmin <fathimanesmi@gmail.com>
2023-10-29 16:29:17 -0400

    movement of the ship towards right and left

--- settings.py
+++ settings.py
@@ -6,3 +6,5 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 700
         self.bg_color = (159, 226, 191)
+        #Speed of the ship
+        self.ship_speed = 1.5
